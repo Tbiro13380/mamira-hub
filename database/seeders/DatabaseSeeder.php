704dd77ec23cd310c5ab0a87f3b45cd47a8c19de
Guilherme Tiburcio Ferreira <guilherme.tiburcio.ferreira@gmail.com>
2026-02-02 19:29:39 -0300

    Update package-lock.json for dependency version upgrades and refactor DatabaseSeeder to utilize separate seeders for User and Badge data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,17 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::firstOrCreate(
-            ['email' => 'test@example.com'],
-            array_merge(
-                User::factory()->definition(),
-                [
-                    'name' => 'Test User',
-                    'email' => 'test@example.com',
-                ]
-            )
-        );
+        $this->call([
+            UserSeeder::class,
+            BadgeSeeder::class,
+        ]);
     }
 }
