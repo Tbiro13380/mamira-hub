3766d163f45f68d467717e602d5c9f3e21660eb1
Guilhermeprecode <guilherme.tiburcio@precode.com.br>
2026-02-03 11:30:19 -0300

    Update Vite configuration to disable generateOnBuild option for Wayfinder

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         tailwindcss(),
         wayfinder({
             formVariants: true,
+            generateOnBuild: false,
         }),
         vue({
             template: {
